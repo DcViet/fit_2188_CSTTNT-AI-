ef9e2a63d6f4c59e439104d1de6c7e699d581feb
DcViet <111166640+DcViet@users.noreply.github.com>
2023-06-21 18:11:57 +0700

    Update DataStructure.py

--- Lab 0: Learning Python/DataStructure.py
+++ Lab 0: Learning Python/DataStructure.py
@@ -1,1 +1,10 @@
-
+# Exercise 1: Create a list by picking an odd-index items from the first list and even index items from the second
+# Exercise 2: Remove and add item in a list
+# Exercise 3: Slice list into 3 equal chunks and reverse each chunk
+# Exercise 4: Count the occurrence of each element from a list
+# Exercise 5: Create a Python set such that it shows the element from both lists in a pair
+# Exercise 6: Find the intersection (common) of two sets and remove those elements from the first set
+# Exercise 7: Checks if one set is a subset or superset of another set. If found, delete all elements from that set
+# Exercise 8: Iterate a given list and check if a given element exists as a key’s value in a dictionary. If not, delete it from the list
+# Exercise 9: Get all values from the dictionary and add them to a list but don’t add duplicates
+# Exercise 10: Remove duplicates from a list and create a tuple and find the minimum and maximum number
