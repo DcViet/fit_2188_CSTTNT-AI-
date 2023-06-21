743b3107de88458e2163813fdf6e2e96f5c1d023
DcViet <111166640+DcViet@users.noreply.github.com>
2023-06-21 14:40:56 +0700

    Update Beginners.py

--- Lab 0: Learning Python/Beginners.py
+++ Lab 0: Learning Python/Beginners.py
@@ -1,16 +1,137 @@
 # Exercise 1: Calculate the multiplication and sum of two numbers
-def 
+def multiplication_or_sum(num1, num2):
+    # calculate product of two number
+    product = num1 * num2
+    # check if product is less then 1000
+    if product <= 1000:
+        return product
+    else:
+        # product is greater than 1000 calculate sum
+        return num1 + num2
+
+# first condition
+result = multiplication_or_sum(20, 30)
+print("The result is", result)
+
+# Second condition
+result = multiplication_or_sum(40, 30)
+print("The result is", result) 
 
 # Exercise 2: Print the sum of the current number and the previous number
+print("Printing current and previous number and their sum in a range(10)")
+previous_num = 0
 
+# loop from 1 to 10
+for i in range(1, 11):
+    x_sum = previous_num + i
+    print("Current Number", i, "Previous Number ", previous_num, " Sum: ", x_sum)
+    # modify previous number
+    # set it to the current number
+    previous_num = i
 # Exercise 3: Print characters from a string that are present at an even index number
+# accept input string from a user
+word = input('Enter word ')
+print("Original String:", word)
 
+# get the length of a string
+size = len(word)
+
+# iterate a each character of a string
+# start: 0 to start with first character
+# stop: size-1 because index starts with 0
+# step: 2 to get the characters present at even index like 0, 2, 4
+print("Printing only even index chars")
+for i in range(0, size - 1, 2):
+    print("index[", i, "]", word[i])
 # Exercise 4: Remove first n characters from a string
-# Exercise 5: Check if the first and last number of a list is the same
-# Exercise 6: Display numbers divisible by 5 from a list
-# Exercise 7: Return the count of a given substring from a string
-# Exercise 8: Print the following pattern
-# Exercise 9: Check Palindrome Number
-# Exercise 10: Create a new list from a two list using the following condition
+def remove_chars(word, n):
+    print('Original string:', word)
+    x = word[n:]
+    return x
 
+print("Removing characters from a string")
+print(remove_chars("pynative", 4))
+print(remove_chars("pynative", 2))
+
+# Exercise 5: Check if the first and last number of a list is the same
+def first_last_same(numberList):
+    print("Given list:", numberList)
+    
+    first_num = numberList[0]
+    last_num = numberList[-1]
+    
+    if first_num == last_num:
+        return True
+    else:
+        return False
+
+numbers_x = [10, 20, 30, 40, 10]
+print("result is", first_last_same(numbers_x))
+
+numbers_y = [75, 65, 35, 75, 30]
+print("result is", first_last_same(numbers_y))
+
+# Exercise 6: Display numbers divisible by 5 from a list
+num_list = [10, 20, 33, 46, 55]
+print("Given list:", num_list)
+print('Divisible by 5:')
+for num in num_list:
+    if num % 5 == 0:
+        print(num)
+
+# Exercise 7: Return the count of a given substring from a string
+str_x = "Emma is good developer. Emma is a writer"
+# use count method of a str class
+cnt = str_x.count("Emma")
+print(cnt)
+
+# Exercise 8: Print the following pattern
+for num in range(10):
+    for i in range(num):
+        print (num, end=" ") #print number
+    # new line after each row to display pattern correctly
+    print("\n")
+# Exercise 9: Check Palindrome Number
+def palindrome(number):
+    print("original number", number)
+    original_num = number
+    
+    # reverse the given number
+    reverse_num = 0
+    while number > 0:
+        reminder = number % 10
+        reverse_num = (reverse_num * 10) + reminder
+        number = number // 10
+
+    # check numbers
+    if original_num == reverse_num:
+        print("Given number palindrome")
+    else:
+        print("Given number is not palindrome")
+
+palindrome(121)
+palindrome(125)
+
+# Exercise 10: Create a new list from a two list using the following condition
 # Create a new list from a two list using the following condition. Given a two list of numbers, write a program to create a new list such that the new list should contain odd numbers from the first list and even numbers from the second list.
+def merge_list(list1, list2):
+    result_list = []
+    
+    # iterate first list
+    for num in list1:
+        # check if current number is odd
+        if num % 2 != 0:
+            # add odd number to result list
+            result_list.append(num)
+    
+    # iterate second list
+    for num in list2:
+        # check if current number is even
+        if num % 2 == 0:
+            # add even number to result list
+            result_list.append(num)
+    return result_list
+
+list1 = [10, 20, 25, 30, 35]
+list2 = [40, 45, 60, 75, 90]
+print("result list:", merge_list(list1, list2))
