fadf927947adf4abf569c2b92a10f1d15004a873
DcViet <111166640+DcViet@users.noreply.github.com>
2023-06-21 18:23:24 +0700

    Update Set.py

--- Lab 0: Learning Python/Set.py
+++ Lab 0: Learning Python/Set.py
@@ -1,1 +1,9 @@
-
+# Exercise 1: Add a list of elements to a set
+# Exercise 2: Return a new set of identical items from two sets
+# Exercise 3: Get Only unique items from two sets
+# Exercise 4: Update the first set with items that don’t exist in the second set
+# Exercise 5: Remove items from the set at once
+# Exercise 6: Return a set of elements present in Set A or B, but not both
+# Exercise 7: Check if two sets have any elements in common. If yes, display the common elements
+# Exercise 8: Update set1 by adding items from set2, except common items
+# Exercise 9: Remove items from set1 that are not common to both set1 and set2
