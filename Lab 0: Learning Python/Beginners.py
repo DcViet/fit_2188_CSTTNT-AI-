d3361fe933beda7c474c288e30b0ddc68ab06b28
DcViet <111166640+DcViet@users.noreply.github.com>
2023-06-21 14:23:34 +0700

    Update Beginners.py

--- Lab 0: Learning Python/Beginners.py
+++ Lab 0: Learning Python/Beginners.py
@@ -1,1 +1,16 @@
+# Exercise 1: Calculate the multiplication and sum of two numbers
+def 
 
+# Exercise 2: Print the sum of the current number and the previous number
+
+# Exercise 3: Print characters from a string that are present at an even index number
+
+# Exercise 4: Remove first n characters from a string
+# Exercise 5: Check if the first and last number of a list is the same
+# Exercise 6: Display numbers divisible by 5 from a list
+# Exercise 7: Return the count of a given substring from a string
+# Exercise 8: Print the following pattern
+# Exercise 9: Check Palindrome Number
+# Exercise 10: Create a new list from a two list using the following condition
+
+# Create a new list from a two list using the following condition. Given a two list of numbers, write a program to create a new list such that the new list should contain odd numbers from the first list and even numbers from the second list.
