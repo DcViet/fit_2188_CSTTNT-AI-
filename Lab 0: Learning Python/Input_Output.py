a5c0bc1f7abefcd21460a119cee2166eb3820c4d
DcViet <111166640+DcViet@users.noreply.github.com>
2023-06-21 15:35:59 +0700

    Update Input_Output.py

--- Lab 0: Learning Python/Input_Output.py
+++ Lab 0: Learning Python/Input_Output.py
@@ -1,1 +1,10 @@
-
+# Exercise 1: Accept numbers from a user
+# Exercise 2: Display three string “Name”, “Is”, “James” as “Name**Is**James”
+# Exercise 3: Convert Decimal number to octal using print() output formatting
+# Exercise 4: Display float number with 2 decimal places using print()
+# Exercise 5: Accept a list of 5 float numbers as an input from the user
+# Exercise 6: Write all content of a given file into a new file by skipping line number 5
+# Exercise 7: Accept any three string from one input() call
+# Exercise 8: Format variables using a string.format() method.
+# Exercise 9: Check file is empty or not
+# Exercise 10: Read line number 4 from the following file
